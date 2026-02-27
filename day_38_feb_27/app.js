7e0e12185331c1f633f505adc48daf231d90fab7
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-02-27 19:46:48 +0530

    chat

---
+++ day_38_feb_27/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+// static file
+app.use(express.static('public'))
+
+const activeUsers = new Set();
+
+io.on('connection', (socket) => {
+    console.log('Made Socket Connection...');
+
+    socket.on('new user', function (data) {
+        socket.userId = data;
+        activeUsers.add(data);
+        io.emit('new user', [...activeUsers])
+    })
+    socket.on('disconnected', function () {
+        activeUsers.delete(socket.userId);
+        io.emit('user disconnected', socket.userId)
+    })
+    socket.on('chat message', function (data) {
+        io.emit('chat message', data)
+    })
+    socket.on('typing', function (data) {
+        io.emit('typing', data)
+    })
+});
+
+server.listen(4000, () => {
+    console.log('server running...')
+});
